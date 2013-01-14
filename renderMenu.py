039e830cfef2ece5baa0d27ab113b8421b77574d
Alykhan Kanji <github@alykhan.com>
2013-01-13 21:57:40 -0500

    Now using Amazon S3 to store menu data.

--- renderMenu.py
+++ renderMenu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import json, os
+import json, os, requests
+from awsauth import S3Auth
 from datetime import datetime, timedelta
 from pytz import timezone
 
@@ -13,8 +14,10 @@ def renderMenu():
 	waterlooTimezone = timezone('America/Toronto')
 	nowWaterloo = datetime.now(waterlooTimezone)
 	currentDatetime = nowWaterloo.strftime('%I:%M %p on %A, %B %d, %Y')
-	with open('response.txt') as jsonResponseFile:
-		menu = json.load(jsonResponseFile)['response']['data']
+	ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+	SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+	jsonResponseFile = requests.get('http://s3.amazonaws.com/uwfoodmenu/response.txt', auth=S3Auth(ACCESS_KEY, SECRET_KEY))
+	menu = jsonResponseFile.json()['response']['data']
 
 	foodlist = menu['Restaurants'].values()
 	return render_template('index.html', menu=menu, foodlist=foodlist, nowWaterloo=nowWaterloo, currentDatetime=currentDatetime)
