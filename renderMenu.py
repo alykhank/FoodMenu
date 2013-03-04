cd67b7dba3c030da35b2620ff8319921d547e501
Alykhan Kanji <github@alykhan.com>
2013-03-04 10:18:01 -0500

    Pass serviceInfo to template.

--- renderMenu.py
+++ renderMenu.py
@@ -6,16 +6,18 @@ from pytz import timezone
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
+SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+MIXPANEL_TOKEN = os.environ.get('MIXPANEL_TOKEN')
 
-@app.route("/")
+@app.route('/')
 def renderMenu():
 	nowWaterloo = datetime.now(timezone('America/Toronto'))
-	ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
-	SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-	MIXPANEL_TOKEN = os.environ.get('MIXPANEL_TOKEN')
-	r = requests.get('http://s3.amazonaws.com/uwfoodmenu/response.txt', auth=S3Auth(ACCESS_KEY, SECRET_KEY))
-	menu = r.json()['response']['data']
-	return render_template('index.html', menu=menu, nowWaterloo=nowWaterloo, mixpanelToken=MIXPANEL_TOKEN)
+	foodMenu = requests.get('http://s3.amazonaws.com/uwfoodmenu/foodMenu.txt', auth=S3Auth(ACCESS_KEY, SECRET_KEY))
+	menu = foodMenu.json()['response']['data']
+	serviceInfo = requests.get('http://s3.amazonaws.com/uwfoodmenu/serviceInfo.txt', auth=S3Auth(ACCESS_KEY, SECRET_KEY))
+	info = serviceInfo.json()['response']['data']
+	return render_template('index.html', menu=menu, info=info, nowWaterloo=nowWaterloo, mixpanelToken=MIXPANEL_TOKEN)
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
