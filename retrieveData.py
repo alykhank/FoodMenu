db750913a9680156ded64c7f49e11468cfea589a
Alykhan Kanji <github@alykhan.com>
2013-03-04 09:50:15 -0500

    Request Service Info along with Food Menu.

--- retrieveData.py
+++ retrieveData.py
@@ -3,14 +3,15 @@ import json, os, requests
 from awsauth import S3Auth
 
 key = os.environ.get('UWOPENDATA_APIKEY')
-service = 'FoodMenu'
-
-def getMenu():
-	payload = {'key': key, 'service': service}
-	r = requests.get('http://api.uwaterloo.ca/public/v1/', params=payload)
-	return r.text
-
-menu = getMenu()
 ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
 SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+def getData(service):
+	payload = {'key': key, 'service': service}
+	r = requests.get('http://api.uwaterloo.ca/public/v1/', params=payload)
+	return r
+
+menu = getData('FoodMenu').text
 requests.put('http://s3.amazonaws.com/uwfoodmenu/response.txt', data=menu, auth=S3Auth(ACCESS_KEY, SECRET_KEY))
+serviceInfo = getData('FoodServices').text
+requests.put('http://s3.amazonaws.com/uwfoodmenu/serviceInfo.txt', data=serviceInfo, auth=S3Auth(ACCESS_KEY, SECRET_KEY))
