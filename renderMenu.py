6dd1881fc2631602d7e34aede208abf42ed688aa
Alykhan Kanji <github@alykhan.com>
2013-04-02 11:24:50 -0400

    Add API endpoints to serve data in JSON format.

--- renderMenu.py
+++ renderMenu.py
@@ -3,7 +3,7 @@ import json, os, requests
 from awsauth import S3Auth
 from datetime import datetime
 from pytz import timezone
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
 from models import app, db, FoodMenu, FoodServices
 
 MIXPANEL_TOKEN = os.environ.get('MIXPANEL_TOKEN')
@@ -16,6 +16,18 @@ def renderMenu():
 	serviceInfo = FoodServices.query.order_by(FoodServices.id.desc()).first().result
 	locations = json.loads(serviceInfo)['response']['data']
 	return render_template('index.html', menu=menu, locations=locations, nowWaterloo=nowWaterloo, mixpanelToken=MIXPANEL_TOKEN)
+
+@app.route('/foodmenu')
+def foodmenu():
+	foodMenu = FoodMenu.query.order_by(FoodMenu.id.desc()).first().result
+	menu = json.loads(foodMenu)['response']['data']
+	return jsonify(menu)
+
+@app.route('/foodservices')
+def foodservices():
+	serviceInfo = FoodServices.query.order_by(FoodServices.id.desc()).first().result
+	locations = json.loads(serviceInfo)['response']['data']
+	return jsonify(locations)
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
