6d4cddc22b4281c18c96fc0aa80d55ade3601ef4
Alykhan Kanji <github@alykhan.com>
2015-03-21 14:24:30 -0700

    Update datetime filters, show closed dates

--- uwmenu.py
+++ uwmenu.py
@@ -4,8 +4,8 @@
 
 import json
 import os
-
 from datetime import datetime
+
 from pytz import timezone
 from flask import render_template, jsonify
 
@@ -35,11 +35,25 @@ def menu_api():
     menu = json.loads(food_menu)['data']
     return jsonify(menu)
 
-def datetimeformat(
-        value, full_date_format='%Y-%m-%d', month_day_format='%B %d'):
+def fulldateformat(value,
+                   full_date_format="%Y-%m-%d",
+                   friendly_format="{date.day}/{date.month}/{date.year}"):
+    """Convert datetimes from 'Y-m-d' to 'd/m/Y'."""
+    date = datetime.strptime(value, full_date_format)
+    return friendly_format.format(date=date)
+def dateformat(value,
+               full_date_format="%Y-%m-%d",
+               month_day_format="{date:%B} {date.day}"):
     """Convert datetimes from 'Y-m-d' to 'B d'."""
-    value_date = datetime.strptime(value, full_date_format)
-    return value_date.strftime(month_day_format)
+    date = datetime.strptime(value, full_date_format)
+    return month_day_format.format(date=date)
+
+def timeformat(value,
+               full_time_format="%H:%M",
+               friendly_format="{time:%l}:{time.minute:02} {time:%p}"):
+    """Convert datetimes from 'H:M' to 'I:M p'."""
+    time = datetime.strptime(value, full_time_format)
+    return friendly_format.format(time=time)
 
 def building_info(outlet_id, locations):
     """Find building name for given outlet ID."""
@@ -50,6 +64,9 @@ def building_info(outlet_id, locations):
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
     PORT = int(os.environ.get('PORT', 5000))
-    app.jinja_env.filters['datetimeformat'] = datetimeformat
+    app.debug = os.environ.get('DEBUG')
+    app.jinja_env.filters['fulldateformat'] = fulldateformat
+    app.jinja_env.filters['dateformat'] = dateformat
+    app.jinja_env.filters['timeformat'] = timeformat
     app.jinja_env.filters['building_info'] = building_info
     app.run(host='0.0.0.0', port=PORT)
