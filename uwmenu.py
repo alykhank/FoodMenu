5819305d26650116bc7a6207e1478984cbd5f438
Alykhan Kanji <github@alykhan.com>
2015-03-24 15:29:58 -0700

    Cache aggregated menu object and use for /menu

--- uwmenu.py
+++ uwmenu.py
@@ -12,18 +12,41 @@ import requests
 
 MIXPANEL_TOKEN = os.environ.get('MIXPANEL_TOKEN')
 KEY = os.environ.get('UWOPENDATA_APIKEY')
+MENU_ENDPOINT = "menu.json"
+LOCATIONS_ENDPOINT = "locations.json"
+OUTLETS_ENDPOINT = "outlets.json"
+AGGREGATE_MENU = "foodservices"
 app = Flask(__name__)
 cache = redis.from_url(os.environ.get('REDISTOGO_URL', 'redis://localhost:6379'))
 
 def retrieve(service):
     """Request service data from UW Open Data API as JSON HTTPResponse text."""
-    resp = cache.get(service)
-    if resp is None:
-        payload = {"key": KEY}
-        url = "https://api.uwaterloo.ca/v2/foodservices/" + service
-        resp = requests.get(url, params=payload).text
-        cache.setex(service, resp, 60) # expire cached value after 60 seconds
+    payload = {"key": KEY}
+    url = "https://api.uwaterloo.ca/v2/foodservices/" + service
+    resp = requests.get(url, params=payload).text
     return resp
+
+def retrieve_all_outlet_details():
+    """Retrieve menu, locations, and outlets data then aggregate into single object."""
+    menu = cache.get(AGGREGATE_MENU)
+    if menu:
+        menu = json.loads(menu)
+    else:
+        menu = json.loads(retrieve(MENU_ENDPOINT))['data']
+        locations = json.loads(retrieve(LOCATIONS_ENDPOINT))['data']
+        outlets = json.loads(retrieve(OUTLETS_ENDPOINT))['data']
+        for eatery in menu['outlets']:
+            for location in locations:
+                if eatery['outlet_id'] == location['outlet_id']:
+                    eatery['location'] = location
+            for outlet in outlets:
+                if eatery['outlet_id'] == outlet['outlet_id']:
+                    daily_meals = []
+                    if outlet['has_breakfast']: daily_meals += ['Breakfast']
+                    if outlet['has_lunch']: daily_meals += ['Lunch']
+                    if outlet['has_dinner']: daily_meals += ['Dinner']
+                    eatery['meals'] = daily_meals
+    return menu
 
 def attach_filters():
     """Attach Jinja filters to app for use in templates."""
@@ -34,26 +57,13 @@ def attach_filters():
 @app.route('/')
 def index():
     """Send menu and location data to templates."""
-    menu = json.loads(retrieve('menu.json'))['data']
-    locations = json.loads(retrieve('locations.json'))['data']
-    outlets = json.loads(retrieve('outlets.json'))['data']
-    for eatery in menu['outlets']:
-        for location in locations:
-            if eatery['outlet_id'] == location['outlet_id']:
-                eatery['location'] = location
-        for outlet in outlets:
-            if eatery['outlet_id'] == outlet['outlet_id']:
-                daily_meals = []
-                if outlet['has_breakfast']: daily_meals += ['Breakfast']
-                if outlet['has_lunch']: daily_meals += ['Lunch']
-                if outlet['has_dinner']: daily_meals += ['Dinner']
-                eatery['meals'] = daily_meals
+    menu = retrieve_all_outlet_details()
     return render_template('index.html', menu=menu, mixpanelToken=MIXPANEL_TOKEN)
 
 @app.route('/menu')
 def menu_api():
     """Serve menu data as JSON API."""
-    menu = json.loads(retrieve('menu.json'))['data']
+    menu = retrieve_all_outlet_details()
     return jsonify(menu)
 
 def fulldateformat(value,
